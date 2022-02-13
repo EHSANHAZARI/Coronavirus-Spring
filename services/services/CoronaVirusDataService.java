df79fc28532c418bd80caee5e3372d12bc2c9dec
EHSANHAZARI <57685610+EHSANHAZARI@users.noreply.github.com>
2022-02-12 19:27:26 -0500

    Update CoronaVirusDataService.java

--- services/services/CoronaVirusDataService.java
+++ services/services/CoronaVirusDataService.java
@@ -27,16 +27,19 @@ public class CoronaVirusDataService {
     }
 
     @PostConstruct
-    @Scheduled(cron = "* * 1 * * * ")
-    public void FetchVirusData() throws IOException, InterruptedException {
-        List<LocationStats> newStats= new ArrayList<>();
+    @Scheduled(cron = "* * 1 * * * ") // when you want some functionality getting repeated by a schedule
+    public void FetchVirusData() throws IOException, InterruptedException { // this method fetch the data that have been provided by uri
+        List<LocationStats> newStats= new ArrayList<>(); // we make newStats because we want our application works while we are performing some other task without any delay
+        // This is showing how we request to get the information in the specific uri
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(VIRUS_DATA_URL)).build();
         HttpResponse<String>httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
         StringReader csvreader = new StringReader(httpResponse.body());
+        // Apache common csv provides different method for reading the file we are using default here 
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvreader);
         for (CSVRecord record : records) {
             LocationStats locationStat = new LocationStats();
+            // we are getting the information with these specific title and store them in locationStat
             locationStat.setState(record.get("Province/State"));
             locationStat.setCountry(record.get("Country/Region"));
             locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
